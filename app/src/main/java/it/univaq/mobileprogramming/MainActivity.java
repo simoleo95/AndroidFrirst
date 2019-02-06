e8c741b0378d8db9cd23d379febf5454141f1af3
simoleo95 <leonardisimonemaria@gmail.com>
2019-02-06 15:18:26 +0100

    Initial commit

---
+++ app/src/main/java/it/univaq/mobileprogramming/MainActivity.java
@@ -0,0 +1,37 @@
+package it.univaq.mobileprogramming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        City[] data = new City[3];
+
+        City c1 = new City("Roma", "Lazio");
+        data[0] = c1;
+
+        City c2 = new City("L'Aquila", "Abruzzo");
+        data[1] = c2;
+
+        City c3 = new City("Firenze", "Toscana");
+        data[2] = c3;
+
+
+        // Adapter adapter = new Adapter(data);
+        AdapterRecycler adapter = new AdapterRecycler(data);
+
+        RecyclerView list = findViewById(R.id.main_list);
+
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+        list.setAdapter(adapter);
+    }
+}
