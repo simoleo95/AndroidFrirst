0cf61bf80104753adfdab44b3e5935e69c5665ae
Marco Carolla <chiocciolalibero.eu@libero.it>
2019-03-20 10:46:11 +0100

    Creata seconda Activity per mostrare i risultati!!!

--- app/src/main/java/it/univaq/mobileprogramming/utility/U_Vars.java
+++ app/src/main/java/it/univaq/mobileprogramming/utility/U_Vars.java
@@ -19,11 +19,7 @@ public class U_Vars
     /** MyReceiver - Finished using GPS and located the user*/
     public static boolean userHasBeenLocated = false;
     
-    /** Switch between loading and show all Pharmacies in user's city */
-    public static boolean loadingDone = false;
-    
-    /** Show pharms list */
-    public static boolean canShowListNow = false;
+    public static String userCity;
     
     public static List<E_Farmacia> farmacieUtente;
 }
